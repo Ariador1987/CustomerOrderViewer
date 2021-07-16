e93cc4fe76ca012153c47fa1c9e6f9032dd5924e
Božidar Bralić <bozidar.bralic@gmail.com>
2021-07-16 22:37:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using CustomerOrderViewer.Models;
+using CustomerOrderViewer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connString = "Data Source=.\\SQLEXPRESS;Initial Catalog=CustomerOrderViewer;Integrated Security=True";
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(connString);
+
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())
+                {
+                    foreach (CustomerOrderDetailModel customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine(@$"Id: {customerOrderDetailModel.CustomerOrderId} Fullname: {customerOrderDetailModel.FirstName} {customerOrderDetailModel.LastName} - purchased: {customerOrderDetailModel.Description} for the price: {customerOrderDetailModel.Price}.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong {ex.Message}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
